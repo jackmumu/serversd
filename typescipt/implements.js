12bd9cc13e3f32bd16773d95e9cbb9bc5c935ba1
刘庆林 <1459307172@qq.com>
2021-07-22 16:44:11 +0800

    feat

--- typescipt/implements.js
+++
@@ -1,9 +0,0 @@
-var Agriloan = /** @class */ (function () {
-    function Agriloan(interest, rebate) {
-        this.interest = interest;
-        this.rebate = rebate;
-    }
-    return Agriloan;
-}());
-var obj4 = new Agriloan(10, 1);
-console.log("利润为:" + obj4.interest + ",抽象为：" + obj4.rebate);
--- typescipt/src/app.ts
+++ typescipt/src/app.ts
@@ -1,1 +1,6 @@
-console.log("hello")
+import {hi} from "./m1"
+function num(aa:number,bb:number):number{
+    return aa+bb
+}
+console.log(num(11,22))
+console.log(hi)
